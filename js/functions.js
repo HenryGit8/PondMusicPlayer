1f34eecebe8e31d72764ff9cddc1106f0513204f
Heyouchi <982907295@qq.com>
2019-01-13 13:22:16 +0800

    尝试跨域

--- js/functions.js
+++ js/functions.js
@@ -423,8 +423,8 @@ function openDownloadDialog(url, saveName)
     });*/
     //download(url, saveName,"audio/mp3")
 
-    /*var aLink = document.createElement('a');
-    aLink.href = url;
+    var aLink = document.createElement('a');
+    aLink.href = "/imgProxy?url="+url;
     aLink.target = "_blank";
     aLink.download = saveName || ''; // HTML5新增的属性，指定保存文件名，可以不要后缀，注意，file:///模式下不会生效
     var event;
@@ -434,7 +434,7 @@ function openDownloadDialog(url, saveName)
         event = document.createEvent('MouseEvents');
         event.initMouseEvent('click', true, false, window, 0, 0, 0, 0, 0, false, false, false, false, 0, null);
     }
-    aLink.dispatchEvent(event);*/
+    aLink.dispatchEvent(event);
 }
 window.downloadFile = function (sUrl) {
 
