45bac84e10aaf20a9c4d9e523d70c88497f926f6
Heyouchi <982907295@qq.com>
2019-01-13 13:23:45 +0800

    删除代码

--- js/functions.js
+++ js/functions.js
@@ -397,15 +397,6 @@ function openDownloadDialog(url, saveName)
     {
         url = URL.createObjectURL(url); // 创建blob地址
     }
-    //window.open(url);
-    var $eleForm = $("<form method='get'></form>");
-
-    $eleForm.attr("action",url);
-
-    $(document.body).append($eleForm);
-
-    //提交表单，实现下载
-    $eleForm.submit();
     /*var x=new XMLHttpRequest();
     x.open("GET", url, true);
     x.responseType = 'blob';
